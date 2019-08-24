f407cd2e0cfda618c06b91e9f0dd502fdd240a70
Alexander Koryagin <a.koryagin@analysiscenter.org>
2019-08-25 02:26:24 +0300

    add setup-script

--- pydens/__init__.py
+++ pydens/__init__.py
@@ -1,4 +1,5 @@
-""" Init. """
+""" PyDEns Init-file. """
+
 #pylint: disable=no-name-in-module, import-error
 from .syntax_tree import *
 from .tokens import add_tokens
@@ -6,3 +7,5 @@ from .letters import *
 from .model_tf import TFDeepGalerkin
 from .wrapper import Solver
 from .batchflow.sampler import *
+
+__version__ = '0.1.0'
