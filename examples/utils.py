af1a8806b2fa5ca2798aa418899a2e8daf80e71f
Alexander Koryagin <a.koryagin@analysiscenter.org>
2019-08-26 04:51:06 +0300

    update seminar nb; update plot_utils

--- examples/utils.py
+++ examples/utils.py
@@ -14,13 +14,14 @@ def plot_loss(loss, color='powderblue'):
     plt.show()
 
 
-def plot_pair(solution, model, points=np.linspace(0, 1, 200).reshape(-1, 1), n_params=0,
+def plot_pair(solution, model, points=None, plot_coord=None,
               xlabel=r'$t$', ylabel=r'$\hat{u} | u$', confidence=None, alpha=0.4,
-              title='Solution against approximation', loc=1):
+              title='Solution against approximation', loc=1, grid=True):
     """ Plot solution-approximation given by a pydens-model along with true solution.
     """
+    points = points or np.linspace(0, 1, 200).reshape(-1, 1)
     approxs = model.solve(points)
-    points = points if n_params == 0 else points[:, :points.shape[1] - n_params]
+    points = points if plot_coord is None else points[:, plot_coord]
     true = solution(points).reshape(-1)
     plt.plot(points, true, 'b', linewidth=4, label='True solution', alpha=alpha + 0.15)
     plt.plot(points, approxs, 'r--', linewidth=5, label='Network approximation')
@@ -32,6 +33,7 @@ def plot_pair(solution, model, points=np.linspace(0, 1, 200).reshape(-1, 1), n_p
         plt.fill_between(points.reshape(-1), true - confidence, true + confidence, alpha=alpha,
                          label='Confidence')
     plt.legend(loc=loc)
+    plt.grid(grid)
     plt.show()
 
 
@@ -50,8 +52,29 @@ def show_heatmap(model, fetches=None, grid=None, cmap='viridis',
     plt.colorbar()
     plt.show()
 
+
 def cart_prod(*arrs):
     """ Get array of cartesian tuples from arbitrary number of arrays.
     """
     grids = np.meshgrid(*arrs, indexing='ij')
     return np.stack(grids, axis=-1).reshape(-1, len(arrs))
+
+
+def plot_sections(model, timestamps=(0, 0.2, 0.4, 0.6, 0.7, 0.9), grid_size=(2, 3), points=None,
+                  fetches=None, ylim=(0, 0.3), title=r'Heat PDE in $\mathcal{R}$: $\hat{u}$'):
+    """ Plot 1d-sections of an approximation to an evolution equation.
+    """
+    points = points or np.linspace(0, 1, 100)
+    n_sections = len(timestamps)
+    fig, axes = plt.subplots(*grid_size, figsize=(5 * grid_size[1], 5))
+    for i, t_ in enumerate(timestamps):
+        points_ = np.concatenate([points.reshape(-1, 1), t_ * np.ones((points.shape[0], 1))], axis=1)
+        wx, wy = i // grid_size[1], i % grid_size[1]
+        axes[wx, wy].plot(points.reshape(-1), model.solve(points_, fetches=fetches))
+        axes[wx, wy].set_ylim(*ylim)
+        axes[wx, wy].set_title('$t=%.2f$' % t_, size=22)
+
+    fig.tight_layout()
+    fig.suptitle(title, size=27)
+    fig.subplots_adjust(top=0.82)
+    fig.show()
