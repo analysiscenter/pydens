f92f9e212a036c156c1d1bac0dd27e956e344d75
Alexander Koryagin <a.koryagin@analysiscenter.org>
2019-09-06 00:36:36 +0300

    update plotter

--- examples/utils.py
+++ examples/utils.py
@@ -60,21 +60,45 @@ def cart_prod(*arrs):
     return np.stack(grids, axis=-1).reshape(-1, len(arrs))
 
 
-def plot_sections(model, timestamps=(0, 0.2, 0.4, 0.6, 0.7, 0.9), grid_size=(2, 3), points=None,
-                  fetches=None, ylim=(0, 0.3), title=r'Heat PDE in $\mathcal{R}$: $\hat{u}$'):
+def plot_sections(model, mode='2d', timestamps=(0, 0.2, 0.4, 0.6, 0.7, 0.9), grid_size=(2, 3), points=None,
+                  fetches=None, ylim=(0, 0.3), zlim=None, title=r'Heat PDE in $\mathcal{R}$: $\hat{u}$'):
     """ Plot 1d-sections of an approximation to an evolution equation.
     """
-    points = points if points is not None else np.linspace(0, 1, 100).reshape(-1, 1)
-    n_sections = len(timestamps)
-    fig, axes = plt.subplots(*grid_size, figsize=(5 * grid_size[1], 5))
-    for i, t_ in enumerate(timestamps):
-        points_ = np.concatenate([points.reshape(-1, 1), t_ * np.ones((points.shape[0], 1))], axis=1)
-        wx, wy = i // grid_size[1], i % grid_size[1]
-        axes[wx, wy].plot(points.reshape(-1), model.solve(points_, fetches=fetches))
-        axes[wx, wy].set_ylim(*ylim)
-        axes[wx, wy].set_title('$t=%.2f$' % t_, size=22)
+    if mode == '2d':
+        points = points if points is not None else np.linspace(0, 1, 100).reshape(-1, 1)
+        n_sections = len(timestamps)
+        fig, axes = plt.subplots(*grid_size, figsize=(5 * grid_size[1], 5))
+        for i, t_ in enumerate(timestamps):
+            points_ = np.concatenate([points.reshape(-1, 1), t_ * np.ones((points.shape[0], 1))], axis=1)
+            wx, wy = i // grid_size[1], i % grid_size[1]
+            axes[wx, wy].plot(points.reshape(-1), model.solve(points_, fetches=fetches))
+            axes[wx, wy].set_ylim(*ylim)
+            axes[wx, wy].set_title('$t=%.2f$' % t_, size=22)
 
-    fig.tight_layout()
-    fig.suptitle(title, size=27)
-    fig.subplots_adjust(top=0.82)
-    fig.show()
+        fig.tight_layout()
+        fig.suptitle(title, size=27)
+        fig.subplots_adjust(top=0.82)
+        fig.show()
+
+    elif mode == '3d':
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure(figsize=(grid_size[0]*7, grid_size[1]*3))
+        x = points if points is not None else np.linspace(0, 1, 30)
+        y = points if points is not None else np.linspace(0, 1, 30)
+        X, Y = np.meshgrid(x, y)
+        points = cart_prod(x, y)
+
+        for i, t_ in enumerate(timestamps):
+            points_ = np.concatenate([points, t_ * np.ones((points.shape[0], 1))], axis=1)
+            ax = fig.add_subplot(*grid_size, i + 1, projection='3d')
+            Z = model.solve(points_).reshape(len(x), len(y))
+            ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                            cmap='viridis', edgecolor='none')
+            ax.set_title('$t=%.2f$' % t_, size=17);
+            ax.set_xlabel(r'$x$', fontdict={'fontsize': 14})
+            ax.set_ylabel(r'$y$', fontdict={'fontsize': 14})
+            ax.set_zlim(*zlim)
+
+        fig.suptitle(title, size=20)
+        plt.savefig('test.png', dpi=400)
+        fig.show()
