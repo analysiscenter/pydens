97453f6d6d39c41f7c7c4369149e29f7baebe2b0
alexander koryagin <akoriagin@nes.ru>
2021-12-20 18:59:47 +0300

    remove tf

--- pydens/__init__.py
+++ pydens/__init__.py
@@ -1,12 +1,7 @@
 """ PyDEns Init-file. """
 
 #pylint: disable=no-name-in-module, import-error
-from .syntax_tree import *
-from .tokens import add_tokens
-from .letters import *
-from .model_tf import TFDeepGalerkin
-from .wrapper import Solver
-from .plot_utils import *
+from .model_torch import Solver, D, V, CustomModel, TorchModel
 from .batchflow.sampler import *
 
 __version__ = '0.1.2'
