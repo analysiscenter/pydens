6e6ad88442edb87370eb77d274c12d241c7da04f
alexander koryagin <akoriagin@nes.ru>
2022-01-19 15:31:53 +0300

    update docs

--- pydens/model_torch.py
+++ pydens/model_torch.py
@@ -44,6 +44,18 @@ class TorchModel(ABC, nn.Module):
 
     def freeze_trainable(self, layers=None, variables=None):
         """ Freeze layers and trainable variables.
+
+        Parameters
+        ----------
+        layers : tuple or list
+            Contains names of layers that need not be trained during the run of `Solver.fit`.
+            In other words, frozen.
+        variables : tuple or list
+            Contains names of variables that need to be frozen.
+
+            Examples:
+
+            - ``variables=['log_scale'] # Frozes trainable multiplier that is included in the anzatc.``
         """
         layers = layers or []
         variables = variables or []
@@ -59,7 +71,14 @@ class TorchModel(ABC, nn.Module):
             param.requires_grad = False
 
     def unfreeze_trainable(self, layers=None, variables=None):
-        """ Unfreeze layers and trainable variables.
+        """ Unfreeze layers and trainable variables. Reverses the effect of method `TorchModel.freeze_trainable`.
+
+        Parameters
+        ----------
+        layers : tuple or list
+            Names of layers that will be made trainable again.
+        variables : tuple or list
+            Names of variables that will be made trainable again.
         """
         layers = layers or []
         variables = variables or []
@@ -93,8 +112,30 @@ class TorchModel(ABC, nn.Module):
         return u
 
 class ConvBlockModel(TorchModel):
-    """ Model that uses capabilities of batchflow.models.torch.conv_block. Can create
-    large family of neural networks in a line of code.
+    """ Model that can create large family of neural networks in a line of code.
+    The class is based on "`convolutional block from batchflow <https://github.com/analysiscenter/batchflow>`_".
+
+    The class allows to easily implement fully connected neural networks as well as convolutional
+    neural networks. For purposes of solving simple differential equations we suggest using
+    fully connected networks with skip connections.
+
+    Parameters
+    ----------
+    layout : str
+        String defining the sequence of layers - for instance, 'fa R fa + f'.
+        Letter 'f' stands for fully connected layer while letter 'c' - for convolutional layer;
+        'a' inserts activation, 'R' defines the start of the skip connection, while '+' shows
+        where the skip ends through sum-operation.
+    units : sequence
+        Sequence configuring the amount of units in all dense layers of the architecture,
+        if any present in the layout.
+    activation : sequence
+        Sequence of callables, str, for instance: [torch.Sin, torch.nn.Sigmoid, 'Sigmoid'].
+
+    Examples:
+
+        - ``layout, units, activation = 'fa fa f', [5, 10, 1], 'Sigmoid' # Fully-conn with 2 hidden``
+        - ``layout, units, activation = 'faR fa fa+ f', [5, 10, 5, 1], 'Sigmoid # Fully-conn with 3 hidden and skip'``
     """
     def __init__(self, layout='fafaf', units=(20, 30, 1), activation='Sigmoid', **kwargs):
         super().__init__(**kwargs)
@@ -156,15 +197,59 @@ class Solver():
             def pde(f, x, y):
                 return D(D(f, x), x) + D(D(f, y), y) - torch.sin(np.pi * (x + y))
 
+        - .. code-block:: python
+
+            def pde(f, x, e):
+                return D(f, x) - e * np.pi * torch.cos(e * np.pi * x)
+
+            Note that the last example doesn't include differentiation with respect
+            to `e`. This means that `e` is a parameter with uncertainty rather than a variable.
+            So, the value of `nparams`-parameter should be set to 1, whie the value of
+            `ndims` - to 2.
+
     model : class
         Class inheriting `TorchModel`. The default value is `ConvBlockModel`. The class allows
-        to implement fylly connected/convolutional architectures with multiple branches
+        to implement fully connected/convolutional architectures with multiple branches
         and skip-connections. All of the `kwargs` supplied into `Solver`-initialization
         go into the `model`-class.
 
         Note:
         `ConvBlockModel` is based on `ConvBlock` from framework
         "`BatchFlow <https://github.com/analysiscenter/batchflow>`_".
+
+    constraints : sequence or callable
+        Either sequence of callables or one callable. Each callable is an additional constraint
+        that can be used during `fit`-run to form loss function.
+
+        Examples:
+
+        - ``constraints=lambda f, x: f(0.5) + 1``
+            This constraints allows to bind `f(0.5) = -1` for a simple ODE.
+        - ``constraints=(lambda f, x: f(0.5) + 1, lambda f, x: f(x)**2)``
+            This set of additional constraints can be used to (i) bind `f(0.5) = -1`
+            along with minimization of a L2-norm of `f`.
+
+    ndims : int
+        The dimensionality of the problem. Equals the number of variables. For instance,
+        when the problem includes `x`, `y` and `t`, the dimensionality is equal to 3.
+
+    nparams : int
+        The number of parameters with uncertainty in the model.
+
+    initial_condition : callable or float
+        Defines initial condition for a problem that includes time-variable.
+
+        Examples:
+
+        - ``lambda x: x * (1 - x)``
+            Can define initial condition for a wave equation for a pertubed string with `x` and `t`
+            - variables. The initial condition is basically a perturbation of a string at t=0.
+        - ``lambda x, y: 10 * x * y * (1 - x) * (1 - y)``
+            Can define initial condition for a heat equation describing temperature evolution of a
+            2d plate.
+
+    boundary_condition : float
+        Defines boundary condition for the problem.
 
     kwargs : dict
         Keyword-arguments used for initialization of the model-instance. When `model`-parameter
@@ -215,7 +300,21 @@ class Solver():
 
     @classmethod
     def reshape_and_concat(cls, tensors):
-        """ Cast, reshape and concatenate sequence of incoming tensors. """
+        """ Cast, reshape and concatenate sequence of incoming tensors. Returns `torch.Tensor`
+        of size (N X D).
+
+        Parameters
+        ----------
+        tensors : sequence
+            Sequence of elements. Each element can be an numpy-array, `torch.Tensor` or a number.
+            The function either casts or tiles and casts each array to a `torch.Tensor` of shape (N X 1).
+
+        Returns
+        -------
+        torch.Tensor
+            Tensor of shape (N X D), where D is defined as maximum length of tensor in the incoming
+            sequence.
+        """
         # Determine batch size as max-len of a tensor in included in the sequence.
         xs = list(tensors)
         sizes = ([np.prod(tensor.shape) for tensor in xs if isinstance(tensor, (np.ndarray, torch.Tensor))] +
@@ -237,8 +336,59 @@ class Solver():
         return torch.cat(xs, dim=1)
 
     def fit(self, niters, batch_size, sampler=None, loss_terms='equation', optimizer='Adam',
-            criterion=nn.MSELoss(), lr=0.001, **kwargs):
-        """ Fit the model inside the solver-instance. """
+            criterion=nn.MSELoss(), lr=0.005, **kwargs):
+        """ Perform fit procedure. Trains the model attributed to the `Solver`-instance.
+
+        Parameters
+        ----------
+        niters : int
+            Number of iterations of optimization.
+
+        batch_size : int
+            The number of points sampled for each iteration.
+
+        sampler : Sampler or None
+            An object that generates batches of points for the training prosedure.
+            Shoild contain `sample`-method that accepts `size`-argument. In its turm, the method `sample`
+            should produce the requested number of nd-points. Usually, `sampler`-object is an instance
+            of class `Sampler` from framework "`BatchFlow <https://github.com/analysiscenter/batchflow>`_".
+
+        loss_terms : str/sequence of str
+            Defines loss terms used for constructing total loss-function. Each item from the sequence can be
+            equal to:
+            - 'equation' - the term penalizes the difference between l.h.s and r.h.s of the equation. The default value
+                of the parameter. Should be used in all cases, where one runs `fit` to better solve the equation.
+            - 'constraint_{k}' - the value 'constraint_0' adds the first additional constraint to the total loss,
+                while 'constraint_1' penalizes the second etc.; whenever used, `constraints`-argument should be
+                supplied during the `Solver`-initialization.
+
+        optimizer : str
+            Name of an optimizer from `torch.optim`. If set to `None`, uses already existing optimizer.
+            Whenever supplied, creates a new optimizer-instance.
+            See "`Docstring of torch.optim <https://pytorch.org/docs/stable/optim.html>`_".
+
+        criterion : callable
+            Criterion-function. Accepts two arguments; is used to form terms of the total
+            loss-function. Frequently is a method from `torch.nn`.
+
+            Examples:
+
+            - .. code-block:: python
+
+                kwargs = {criterion=torch.nn.MSELoss(),
+                          loss_terms=('equation', 'constraint_0')}
+                solver.fit(**kwargs)
+
+                # Yields loss given by
+                # [MSELoss(l.h.s of the equation - r.h.s of the equation) + MSELoss(constraint_0(f, x, ...))].
+                # In other words, this loss penalizes both requested terms using supplied criterion.
+
+        lr : float
+            Starting learning rate for the optimizer-initialization.
+
+        kwargs : dict
+            Other keyword-arguments for the the optimizer-initialization.
+        """
         # Initialize the optimizer if supplied.
         if optimizer is not None:
             self.optimizer = getattr(torch.optim, optimizer)([p for p in self.model.parameters()
@@ -288,8 +438,19 @@ class Solver():
             self.losses.append(loss.detach().cpu().numpy())
 
     def predict(self, *xs):
-        """ Get approximation to a solution in a set of points.
-        Points are given by a list of tensors.
+        """ Get predictions of the solution to the problem in supplied points `*xs`. Each
+        `x` is a tensor/array/number that the method will either cast (or cast and tile) to a (1 X D) tensor.
+
+        Parameters
+        ----------
+        tensors : sequence
+            Sequence of elements. Each element can be an numpy-array, `torch.Tensor` or a number.
+            The function either casts (or tiles and casts) each array to a `torch.Tensor` of shape (N X 1).
+
+        Returns
+        -------
+        np.ndarray
+            Array containing solution-prediction.
         """
         # Reshape and concat.
         xs = self.reshape_and_concat(xs)
